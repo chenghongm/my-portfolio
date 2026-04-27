5bba52ebe88fbd871b115fff67ccf59c4d597a71
chenghongm <chenghongm.1@gmail.com>
2026-04-27 14:31:21 -0700

    update parameter name

--- src/pages/api/activities.js
+++ src/pages/api/activities.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.mybase_SUPABASE_URL,
+  process.env.mybase_SUPABASE_SERVICE_ROLE_KEY
 );
 
 export async function POST(req) {
